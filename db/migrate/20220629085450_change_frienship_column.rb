4d1eba3d4b86f28df01c6fb1122f9cfe0bace1d3
diepanh04 <ngdiepanh2002@gmail.com>
2022-07-01 23:36:26 +0700

    delete migrations

--- db/migrate/20220629085450_change_frienship_column.rb
+++
@@ -1,6 +0,0 @@
-class ChangeFrienshipColumn < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :friendships, :user_id, :friend_a_id
-    rename_column :friendships, :friend_id, :friend_b_id
-  end
-end
