6d4cd5c2734cf472cabe378298c6c5cf56e89301
diepanh04 <ngdiepanh2002@gmail.com>
2022-07-01 23:32:05 +0700

    delete migrations

--- db/migrate/20220628103030_remove_foreign_key.rb
+++
@@ -1,7 +0,0 @@
-class RemoveForeignKey < ActiveRecord::Migration[7.0]
-  def change
-    remove_foreign_key "friend_requests", "friendships"
-    remove_foreign_key "friend_requests", "users"
-    remove_foreign_key "friendships", "users", column: "users_id"
-  end
-end
